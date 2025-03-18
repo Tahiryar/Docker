2070fb70b93f12d7a3e2452ffe34f55d0ffdb200
Tahiryar <tahiryar.251k@gmail.com>
2025-03-18 22:02:04 +0500

    add api in server.js

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World! 🚀");
+app.use(express.json()); // JSON data handle karne ke liye
+
+let users = [
+  { id: 1, name: "Ali" },
+  { id: 2, name: "Aisha" }
+];
+
+// GET: Sabhi users ka data fetch karo
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// POST: Naya user add karo
+app.post("/users", (req, res) => {
+  const newUser = { id: users.length + 1, name: req.body.name };
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// DELETE: Kisi user ko delete karo
+app.delete("/users/:id", (req, res) => {
+  users = users.filter(user => user.id !== parseInt(req.params.id));
+  res.json({ message: "User deleted!" });
 });
 
 app.listen(3000, () => {
